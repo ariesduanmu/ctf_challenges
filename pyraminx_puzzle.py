a47fbb479c001f34d2dff538b0544433a320ec1d
ariesduanmu <ldkj@ldkj.com>
2018-06-27 11:22:32 +0800

    small fix

--- pyraminx_puzzle.py
+++ pyraminx_puzzle.py
@@ -23,7 +23,7 @@ def pyraminxPuzzle(face_colours, moves):
                      "L": [[2, 0], [1, 8], [0, 4]],
                      "l": [[2, 0], [2, 1], [2, 2], [2, 3], [1, 8], [1, 3], [1, 7], [1, 6], [0, 4], [0, 6], [0, 5], [0, 1]],
                      "R": [[3, 0], [0, 8], [1, 4]],
-                     "r": [[3, 0], [3, 1], [3, 2], [3, 3], [0, 3], [0, 8], [0, 7], [0, 6], [1, 4], [1, 6], [1, 5], [1, 1]],
+                     "r": [[3, 0], [3, 1], [3, 2], [3, 3], [0, 8], [0, 3], [0, 7], [0, 6], [1, 4], [1, 6], [1, 5], [1, 1]],
                      "B": [[1, 0], [2, 8], [3, 4]],
                      "b": [[1, 0], [1, 1], [1, 2], [1, 3], [2, 8], [2, 3], [2, 7], [2, 6], [3, 4], [3, 6], [3, 5], [3, 1]]
                      }
@@ -63,51 +63,103 @@ def update_cube(cube, current_positions, current_colors):
 def print_cube(cube):
     print("\n".join(",".join(c) for c in cube))
 
-class TestPyraminx(unittest.TestCase):
+class TestPyraminx(unittest.TestCase): 
     def test1(self):
-        exp_1 = [["Y","Y","Y","Y","R","R","R","R","G"],
-                 ["G","R","O","O","O","G","G","G","G"],
-                 ["Y","O","Y","G","O","O","G","G","Y"],
+        exp_1 = [["Y","Y","Y","Y","R","R","R","R","G"], 
+                 ["G","R","O","O","O","G","G","G","G"], 
+                 ["Y","O","Y","G","O","O","G","G","Y"], 
                  ["R","O","O","R","O","Y","Y","R","R"]]
         colours = ["R",  "G",  "Y",  "O"]
         moves = ["B",  "b'",  "u'",  "R"]
         self.assertEqual(pyraminxPuzzle(colours, moves), exp_1)
 
     def test2(self):
-        exp_2 = [["R","R","R","R","R","R","R","R","R"],
-                 ["G","G","G","G","G","G","G","G","G"],
-                 ["Y","Y","Y","Y","Y","Y","Y","Y","Y"],
+        exp_2 = [["R","R","R","R","R","R","R","R","R"], 
+                 ["G","G","G","G","G","G","G","G","G"], 
+                 ["Y","Y","Y","Y","Y","Y","Y","Y","Y"], 
                  ["O","O","O","O","O","O","O","O","O"]]
         colours = ["R",  "G",  "Y",  "O"]
         moves = ["l",  "l'"]
         self.assertEqual(pyraminxPuzzle(colours, moves), exp_2)
 
     def test3(self):
-        exp_3 = [["Y","O","R","G","G","G","G","G","G"],
-                 ["G","O","G","Y","O","O","Y","Y","Y"],
-                 ["R","G","R","R","O","Y","Y","Y","Y"],
+        exp_3 = [["Y","O","R","G","G","G","G","G","G"], 
+                 ["G","O","G","Y","O","O","Y","Y","Y"], 
+                 ["R","G","R","R","O","Y","Y","Y","Y"], 
                  ["R","R","R","R","O","O","O","O","R"]]
         colours = ["R",  "G",  "Y",  "O"]
         moves = ["l",  "l'",  "u",  "R",  "U'",  "L",  "R'",  "u'",  "l'",  "L'",  "r"]
         self.assertEqual(pyraminxPuzzle(colours, moves), exp_3)
 
     def test4(self):
-        exp_4 = [["R","R","R","G","R","R","G","G","G"],
-                 ["G","O","G","G","O","O","O","G","G"],
-                 ["Y","Y","Y","Y","Y","Y","Y","Y","Y"],
+        exp_4 = [["R","R","R","G","R","R","G","G","G"], 
+                 ["G","O","G","G","O","O","O","G","G"], 
+                 ["Y","Y","Y","Y","Y","Y","Y","Y","Y"], 
                  ["R","R","R","R","O","O","O","O","O"]]
         colours = ["R",  "G",  "Y",  "O"]
         moves = ["r"]
         self.assertEqual(pyraminxPuzzle(colours, moves), exp_4)
 
     def test5(self):
-        exp_5 = [["A","A","A","A","A","A","A","A","A"],
-                 ["B","B","B","B","B","B","B","B","B"],
-                 ["C","C","C","C","C","C","C","C","C"],
+        exp_5 = [["A","A","A","A","A","A","A","A","A"], 
+                 ["B","B","B","B","B","B","B","B","B"], 
+                 ["C","C","C","C","C","C","C","C","C"], 
                  ["D","D","D","D","D","D","D","D","D"]]
         colours = ["A",  "B",  "C",  "D"]
         moves = ["l",  "l'",  "r'",  "r",  "u",  "U",  "u'",  "R'",  "L",  "R",  "L'",  "B'",  "U'",  "b",  "B",  "b'"]
         self.assertEqual(pyraminxPuzzle(colours, moves), exp_5)
+
+    def test6(self):
+        exp_6 = [["E","Y","E","G","Y","Y","R","G","G"], 
+                 ["Y","E","Y","R","E","E","E","R","R"], 
+                 ["G","G","G","Y","R","R","E","E","E"], 
+                 ["R","G","R","R","G","G","Y","Y","Y"]]
+        colours = ["R", "G", "Y", "E"]
+        moves = ["b", "l", "r", "u"]
+        self.assertEqual(pyraminxPuzzle(colours, moves), exp_6)
+        
+    def test7(self):
+        exp_7 = [["E","R","R","R","L","R","R","R","U"], 
+                 ["L","U","U","U","E","U","U","U","R"], 
+                 ["U","L","L","L","R","L","L","L","E"], 
+                 ["R","E","E","E","U","E","E","E","L"]]
+        colours = ["R", "U", "L", "E"]
+        moves = ["U", "B", "R", "L"]
+        self.assertEqual(pyraminxPuzzle(colours, moves), exp_7)
+        
+    def test8(self):
+        exp_8 = [["W","W","D","A","W","W","S","A","A"], 
+                 ["A","D","D","A","D","D","D","A","A"], 
+                 ["S","S","S","S","S","W","A","A","S"], 
+                 ["W","W","W","D","D","S","W","S","D"]]
+        colours = ["W", "A", "S", "D"]
+        moves = ["l", "r'", "U'", "u", "r'", "B", "l'", "b'"]
+        self.assertEqual(pyraminxPuzzle(colours, moves), exp_8)
+        
+    def test9(self):
+        exp_9 = [["W","S","D","S","W","S","S","W","A"], 
+                 ["D","W","A","A","D","A","D","W","A"], 
+                 ["S","A","A","D","S","W","W","S","A"], 
+                 ["W","D","D","W","S","D","A","S","D"]]
+        colours = ["W", "A", "S", "D"]
+        moves = ["B'", "R'", "L", "U'", "B", "r'", "l", "B'", "L'", "r'", "L", "U", "u'",
+                 "U", "B'", "r", "L'", "R", "B", "r", "R'", "R", "U'", "U", "L", "r", "L",
+                 "B'", "U", "B", "R", "R'", "R", "u'", "l", "R'", "R", "B", "R'", "U", "u",
+                 "U", "u'", "B'", "r", "L'", "B'", "R'", "B'", "r", "R'", "r", "L", "R'",
+                 "B", "u", "B'", "B", "L", "U", "B", "B", "L", "R", "B", "R", "u'", "R'",
+                 "B", "u", "u'", "L'", "B", "R'", "l'", "U", "U'", "B", "r", "L'", "B", "r'",
+                 "U", "R", "R'", "u'", "r", "R'", "u'", "r'", "L'", "R'", "r'", "U", "u'",
+                 "B'", "U", "L'", "L'", "B"]
+        self.assertEqual(pyraminxPuzzle(colours, moves), exp_9)
+        
+    def test10(self):
+        exp_10 = [["W","W","W","W","W","W","W","W","W"], 
+                  ["A","A","A","A","A","A","A","A","A"], 
+                  ["S","S","S","S","S","S","S","S","S"], 
+                  ["D","D","D","D","D","D","D","D","D"]]
+        colours = ["W", "A", "S", "D"]
+        moves = ["L", "L", "L", "r'", "r'", "r'", "U", "U'", "U", "U'", "b", "b'", "b'", "b"]
+        self.assertEqual(pyraminxPuzzle(colours, moves), exp_10)
 
 if __name__ == '__main__':
     unittest.main()
