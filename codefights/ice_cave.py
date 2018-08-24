bd004c0ab5ccf1539dd318a775202c303a848ae0
ariesduanmu <aries.duanmu@gmail.com>
2018-08-24 18:37:20 +0800

    semi ice

--- codefights/ice_cave.py
+++ codefights/ice_cave.py
@@ -15,12 +15,39 @@ def iceCave(area, direction):
                      "E":[[m,i] for i in range(n) if area[m][i] != "O"]}
     starts = start_points[direction]
     start_direction = directions[direction]
-    paths = [start2goal(start, goal, area, start_direction) for start in starts]
+    paths = [start2goal(start, goal, area, start_direction) \
+             for start in starts \
+             if start2goal(start, goal, area, start_direction) is not None]
+    if len(paths) == 0:
+        return "impossible"
     path = min(paths, key=lambda x: len(x))
     return "".join(rev_directions[p] for p in path)
 
-def start2goal(start, goal, area, startdirection):
-    pass
+def start2goal(position, goal, area, direction):
+    directions = ((0,1),(0,-1),(1,0),(-1,0))
+    passed = [f"{position[0]},{position[1]}"]
+    found = False
+    step = [direction]
+    while position != goal and len(passed) < len(area) * len(area[0]):
+        next_position = [position[0] + direction[0], position[1] + direction[1]]
+        r, c = next_position
+        if 0 <= r < len(area) and 0 <= c < len(area[0]) and area[r][c] != "O":
+            if area[r][c] == "S":
+                step.append(direction)
+            position = next_position
+        else:
+            for d in directions:
+                if d != direction and d != (direction[0]*(-1), direction[1]*(-1)):
+                    pass
+
+        passed.append(f"")
+
+
+
+    if not found:
+        return None
+    return step
+
 
 
 class TestIceCave(unittest.TestCase): 
