5bb7367bac3cb054e767bda03fac96e3b37cda24
ariesduanmu <aries.duanmu@gmail.com>
2018-08-27 14:58:40 +0800

    update gameOfMath

--- codefights/game_of_math.py
+++ codefights/game_of_math.py
@@ -4,7 +4,7 @@ def gameOfMath(expression):
     expression = expression.split(" ")
     o = set([e for e in expression if not ("0"<=e<="9")])
     if len(o) == 0:
-        return expression[0]
+        return int(expression[0])
     d = {}
     from itertools import permutations
     operations = sorted(list(permutations(o,len(o))))
@@ -12,15 +12,24 @@ def gameOfMath(expression):
         n = expression
         for i in e: 
             n = afterOperate(i, n)
-        for i in range(1,len(e)-1):
+        for i in range(1,len(e)+1):
             x = "".join(e[:i])
             if x not in d:
                 d[x] = [n[0]]
             else:
                 d[x].append(n[0])
-    print(d)        
-    
-    return 0
+    print(d)
+    r = []
+    for i in o:
+        if len(d[i]) == 1:
+            r.append(d[i][0])
+        elif len(d[i]) == 2:
+            r.append(max(d[i]))
+        elif len(d[i]) == 6:
+            r.append(max([min(d[i][j:j+2]) for j in range(0,6,2)]))
+
+    print(r)
+    return min(r)
 
 def afterOperate(operate, expression):
     o = {"+":lambda a,b:a+b,
@@ -32,14 +41,39 @@ def afterOperate(operate, expression):
     while i < len(expression):
         e = expression[i]
         if e == operate:
-            a = next_expression.pop()
-            b = expression[i+1]
-            next_expression.append(o[e](int(a),int(b)))
+            next_expression.append(o[e](int(next_expression.pop()),int(expression[i+1])))
             i += 1
         else:
             next_expression.append(e)
         i += 1
     return next_expression
+
+
+''' 
+'*': [-85, -85],
+
+'*+': [-85], 
+'*+-': [-85], 
+
+'*-': [-85], 
+'*-+': [-85], 
+
+'+': [-85, -1440], 
+
+'+*': [-85], 
+'+*-': [-85], 
+
+'+-': [-1440], 
+'+-*': [-1440], 
+
+'-': [-535, -1440], 
+
+'-*': [-535], 
+'-*+': [-535], 
+
+'-+': [-1440], 
+'-+*': [-1440]
+'''
 
 def test():
     expressions = ["7 - 3 * 5 / 2",
@@ -51,7 +85,8 @@ def test():
                    "7 / 9 / 4 / 2",
                    "9",
                    "5 - 0 * 3 + 0 * 9 - 5 * 0 + 1",
-                   "2 + 3 * 7"]
+                   "2 + 3 * 7",
+                   "5 + 5 - 2 * 1 - 3 - 2 * 5 * 9"]
     for expression in expressions:
         print(f"Expression: {expression}")
         gameOfMath(expression)
@@ -110,6 +145,10 @@ class GameOfMathTest(unittest.TestCase):
         output = 23
         self.assertEqual(gameOfMath(expression), output)
 
+    def test11(self):
+        expression = "5 + 5 - 2 * 1 - 3 - 2 * 5 * 9"
+        output = -37
+        self.assertEqual(gameOfMath(expression), output)
 
 
 if __name__ == "__main__":
