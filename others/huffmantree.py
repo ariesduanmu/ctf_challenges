d48ffb8f21169241b42529e899e194cdaa51a881
ariesduanmu <aries.duanmu@gmail.com>
2018-09-12 18:58:50 +0800

    lzw compress, brain die..

--- others/huffmantree.py
+++ others/huffmantree.py
@@ -19,7 +19,6 @@ class HuffmanTreeNode(BinaryTreeNode):
     def compareTo(other_node):
         return self.weight - other_node.weight
   
-
 def buildTrie(data):
     '''
     Build Huffman Tree
@@ -33,36 +32,6 @@ def buildTrie(data):
         s.sort(key=lambda x:x[0])
     return s[0][1]
 
-def expand(root, data):
-    '''
-    Expand compressed data
-    Args:
-        root: Huffman Tree root
-        data: data to be compressed
-    '''
-    result = ""
-    i = 0
-    while i < len(data):
-        n = root
-        while not n.isLeaf:
-            if data[i] == "1":
-                n = n.right
-            else:
-                n = n.left
-            i += 1
-        result += n.value
-    return result
-
-def compress(root, data):
-    '''
-    Compress data
-    Args:
-        root: Huffman Tree root
-        data: data to be compressed
-    '''
-    builder = build_info(root)
-    return "".join(builder[d] for d in data)
-        
 def build_info(root):
     '''
     Get Huffman Tree code to string
@@ -88,16 +57,52 @@ def build_code(builder, node, s):
     build_code(builder, node.left, s+"0")
     build_code(builder, node.right, s+"1")
 
+
+def expand(root, data):
+    '''
+    Expand compressed data
+    Args:
+        root: Huffman Tree root
+        data: data to be compressed
+    '''
+    result = ""
+    i = 0
+    while i < len(data):
+        n = root
+        while not n.isLeaf:
+            if data[i] == "1":
+                n = n.right
+            else:
+                n = n.left
+            i += 1
+        result += n.value
+    return result
+
+def compress(data):
+    '''
+    Compress data
+    Args:
+        root: Huffman Tree root
+        data: data to be compressed
+    '''
+    root = buildTrie(data)
+    builder = build_info(root)
+    return "".join(builder[d] for d in data), root
+
 def main():
-    text  = "ABRACADABRA!"
-    # Build Huffman Tree
-    root = buildTrie(text)      
+    text  = "it was the age of foolishness"
+    print(f"[*] Input: {text}")
+    print(f"[*] Before compress: {len(text)*8} bits")
+    print(f"compressing...")   
     # Compress Data
-    compressed = compress(root, text)
+    compressed, root = compress(text)
     # Expand Data
     expanded = expand(root, compressed)
-
-    print(text == expanded)
+    
+    print(f"[*] Output: {compressed}")
+    print(f"[*] After compress: {len(compressed)} bits")
+    print(f"[*] Compress rate: {int(len(compressed) / (len(text)*8) * 100)}%")
+    # print(text == expanded)
 
 if __name__ == "__main__":
     main()
