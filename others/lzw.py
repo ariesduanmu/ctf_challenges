a27e54a0148abb50a1b24e7170fd99ba218e8085
ariesduanmu <aries.duanmu@gmail.com>
2018-09-23 01:29:00 +0800

    update lzw decode algo

--- others/lzw.py
+++ others/lzw.py
@@ -23,46 +23,27 @@ def compress(data):
                 pre = p_c
     result += [builder[pre]]
     result += [0x80]
-    print(result)
-    print(builder)
     return result
 
-def generate_builder(data):
-    builder = {d:ord(d) for d in set(data)}
-    n = 0x81
-    pre = None
-    for d in data:
-        if pre is None:
-            pre = d
-        else:
-            p_c = pre+d
-            if p_c not in builder:
-                builder[p_c] = n
-                n += 1
-                pre = d
-            else:
-                pre = p_c
-    return {builder[k]:k for k in builder}
-
 def expand(data):
-    builder = {}
+    builder = {i:chr(i) for i in range(33,127)}
     result = ""
     n = 0x81
-    for i in range(len(data)):
-        d = data[i]
-        if d == 0x80:
+    val = builder[data[0]]
+    i = 1
+    while i < len(data):
+        result += val
+        if data[i] == 0x80:
             break
-        if d < 0x80:
-            result += chr(d)
-        else:
-            if d in builder:
-                result += builder[d]
-            else:
-                builder = generate_builder(result)
-                if d in builder:
-                    result += builder[d]
-                else:
-                    result += builder[data[i-1]] + builder[data[i-1]][0]
+        if data[i] in builder:
+            s = builder[data[i]]
+        if n == data[i]:
+            s = val + val[0]
+
+        builder[n] = val + s[0]
+        n += 1
+        i += 1
+        val = s
     return result
 
 class LZWTest(unittest.TestCase):
