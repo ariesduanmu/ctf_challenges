7fb07f567c68aadd4889fa06c9f4be0841fba7f9
ariesduanmu <aries.duanmu@gmail.com>
2018-09-23 00:17:19 +0800

    add lzw decode

--- others/lzw.py
+++ others/lzw.py
@@ -2,6 +2,8 @@
 '''
 Lempel–Ziv–Welch Compression
 '''
+import unittest
+
 def compress(data):
     builder = {d:ord(d) for d in set(data)}
     n = 0x81
@@ -22,17 +24,64 @@ def compress(data):
     result += [builder[pre]]
     result += [0x80]
     print(result)
+    print(builder)
+    return result
 
+def generate_builder(data):
+    builder = {d:ord(d) for d in set(data)}
+    n = 0x81
+    pre = None
+    for d in data:
+        if pre is None:
+            pre = d
+        else:
+            p_c = pre+d
+            if p_c not in builder:
+                builder[p_c] = n
+                n += 1
+                pre = d
+            else:
+                pre = p_c
+    return {builder[k]:k for k in builder}
 
 def expand(data):
-    pass
+    builder = {}
+    result = ""
+    n = 0x81
+    for i in range(len(data)):
+        d = data[i]
+        if d == 0x80:
+            break
+        if d < 0x80:
+            result += chr(d)
+        else:
+            if d in builder:
+                result += builder[d]
+            else:
+                builder = generate_builder(result)
+                if d in builder:
+                    result += builder[d]
+                else:
+                    result += builder[data[i-1]] + builder[data[i-1]][0]
+    return result
 
+class LZWTest(unittest.TestCase):
+    def test_1(self):
+        data = "AAAAAAAAAAAAAAAAAA"
+        self.assertEqual(expand(compress(data)), data)
 
+    def test_2(self):
+        data = "TOBEORNOTTOBE"
+        self.assertEqual(expand(compress(data)), data)
 
+    def test_3(self):
+        data = "YABBADABBADABBADOO"
+        self.assertEqual(expand(compress(data)), data)
+
+    def test_4(self):
+        data = "ABABABABABABABABAB"
+        self.assertEqual(expand(compress(data)), data)
 
 
 if __name__ == "__main__":
-    data = "ABRACADABRABRABRA"
-    d = [65, 66, 82, 65, 67, 65, 68, 129, 131, 130, 136, 65, 128]
-    # compress(data)
-    expand(d)
+    unittest.main()
